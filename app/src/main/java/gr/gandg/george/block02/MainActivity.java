293e214dffcbbf638ebe6602152e88bbc757616f
george <it01151@uom.edu.gr>
2017-02-04 16:54:29 +0200

    Initial commit

---
+++ app/src/main/java/gr/gandg/george/block02/MainActivity.java
@@ -0,0 +1,35 @@
+package gr.gandg.george.block02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listViewAnimals = (ListView)findViewById(R.id.listView);
+
+        final ArrayAdapter<CharSequence> adapterAnimals =
+                ArrayAdapter.createFromResource(this,
+                        R.array.animalsArray, android.R.layout.simple_list_item_1);
+
+        listViewAnimals.setAdapter(adapterAnimals);
+
+        listViewAnimals.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String message = getString(R.string.toastMessage) + adapterAnimals.getItem(position);
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+            }
+
+        });
+    }
+}
